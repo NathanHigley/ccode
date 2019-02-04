07043f2f8b3bba1332eaf4b606c3584632268ecd
NathanHigley <nathan@jhigley.com>
2019-02-04 10:05:40 -0800

    hello.c part 3

---
+++ c/hello.c
@@ -0,0 +1,13 @@
+// hello.c
+// compile gcc hello.c -o hello.o
+// run ./hello.o
+
+#include <stdio.h>
+
+int main(){
+  printf("Hello, World!\n");
+  printf("  d  u     d         e\n");
+  printf("        m    a       n\n");
+  printf("    d      u    d    e\n");
+  return 0;
+}
